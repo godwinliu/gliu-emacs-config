4861e0b287d31e32bfd89f0516143aa8e9250dfc
Godwin Liu <godwin.liu@gmail.com>
2019-10-06 09:41:17 -0400

    brought master up to date - not sure if this will break anything..

--- init.el
+++ init.el
@@ -40,10 +40,10 @@
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 
-;;(defun my-web-mode-hook ()
-;;  "Hooks for Web mode."
-;;  (setq web-mode-markup-indent-offset 2)
-;;  (linum-mode 1)
-;;  )
-;;(add-hook 'web-mode-hook 'my-web-mode-hook)
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 2)
+  (linum-mode 1)
+  )
+(add-hook 'web-mode-hook 'my-web-mode-hook)
 
